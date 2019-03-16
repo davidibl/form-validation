883b39eb281b52a2eb4982d5381e676ac9ebf61c
David Ibl <david.ibl@xnoname.com>
2019-03-16 10:03:59 +0100

    minor refactoring and correct pending state for all controls.

--- src/app/directives/formValidator.ts
+++ src/app/directives/formValidator.ts
@@ -1,5 +1,5 @@
 import { Directive, forwardRef, OnDestroy } from '@angular/core';
-import { Validator, NG_VALIDATORS, FormGroup } from '@angular/forms';
+import { Validator, NG_VALIDATORS, FormGroup, AbstractControl } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
 import { delay } from 'rxjs/operators/delay';
 import { map } from 'rxjs/operators/map';
@@ -23,16 +23,16 @@ export class FormValidator implements Validator, OnDestroy {
 
     constructor(private _remoteValidationService: RemoteValidationService) { }
 
-    public validate(control: FormGroup) {
-        control.markAsPending();
+    public validate(form: FormGroup) {
+        this.markAllPending(form);
         this.subscription = this._remoteValidationService
-            .validate(control.value)
+            .validate(form.value)
             .pipe(
-                tap(_ => this.resetValidationErrors(control)),
+                tap(_ => this.resetValidationErrors(form)),
                 map(errors => {
                     const errorsObject = errors as RemoteValidationErrors;
                     errorsObject.errors.forEach(error => {
-                        this.setError(control, error);
+                        this.setError(form, error);
                     });
                 })
             )
@@ -52,13 +52,21 @@ export class FormValidator implements Validator, OnDestroy {
         control.setErrors(newErrors);
     }
 
+    private markAllPending(form: FormGroup) {
+        this.forAllControls(form, (control) => control.markAsPending());
+    }
+
     private resetValidationErrors(form: FormGroup) {
+        this.forAllControls(form, (control) => control.setErrors(null));
+    }
+
+    private forAllControls(form: FormGroup, func: (control: AbstractControl) => void) {
         const controls = Object.keys(form.controls)
             .map(key => form.controls[key]);
 
         controls
             .filter(control => !control['controls'])
-            .forEach(control => control.setErrors(null));
+            .forEach(control => func(control));
 
         controls
             .filter(control => !!control['controls'])
